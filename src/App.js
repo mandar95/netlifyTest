b9971231fc22dd18451039b0533c0cf8410cafaa
mandar95 <mandarjaurat@gmail.com>
2020-08-29 15:31:57 +0530

    new2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React new
+          Learn React new 2
         </a>
       </header>
     </div>
